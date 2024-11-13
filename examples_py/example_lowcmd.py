824a57331b82869a1d925039ca35b8c1714860e6
GianniLunardi <gianni.lunardi96@gmail.com>
2024-11-13 14:04:12 +0100

    add plot in the lowcmd example

--- examples_py/example_lowcmd.py
+++ examples_py/example_lowcmd.py
@@ -3,6 +3,8 @@ sys.path.append("../lib")
 import unitree_arm_interface
 import time
 import numpy as np
+import matplotlib.pyplot as plt
+from orc.utils import plot_utils
 
 print("Press ctrl+\ to quit process.")
 
@@ -11,9 +13,15 @@ arm = unitree_arm_interface.ArmInterface(hasGripper=True)
 armModel = arm._ctrlComp.armModel
 arm.setFsmLowcmd()
 
+arm.lowcmd.setZeroKp()
+arm.lowcmd.setZeroKd()
+
 duration = 1000
 lastPos = arm.lowstate.getQ()
 targetPos = np.array([0.0, 1.5, -1.0, -0.54, 0.0, 0.0]) #forward
+
+q, qd, qdd, tau = np.zeros((duration, 6)), np.zeros((duration, 6)), np.zeros((duration, 6)), np.zeros((duration, 6))
+q_des, qd_des, qdd_des, tau_des = np.zeros((duration, 6)), np.zeros((duration, 6)), np.zeros((duration, 6)), np.zeros((duration, 6))
 
 for i in range(0, duration):
     arm.q = lastPos*(1-i/duration) + targetPos*(i/duration)# set position
@@ -25,8 +33,61 @@ for i in range(0, duration):
     arm.setGripperCmd(arm.gripperQ, arm.gripperQd, arm.gripperTau)
     arm.sendRecv()# udp connection
     # print(arm.lowstate.getQ())
+
+    # Log 
+    # print(type(arm.lowstate.getQ()))
+    q[i] = arm.lowstate.getQ()
+    qd[i] = arm.lowstate.getQd()
+    qdd[i] = arm.lowstate.getQdd()
+    tau[i] = arm.lowstate.getQTau()
+
+    q_des[i] = arm.q
+    qd_des[i] = arm.qd
+    tau_des[i] = arm.tau
+
     time.sleep(arm._ctrlComp.dt)
 
 arm.loopOn()
 arm.backToStart()
 arm.loopOff()
+
+# Plots
+t = np.linspace(0, duration*2e-3, duration)
+
+fig, ax = plot_utils.create_empty_figure(3, 2)
+ax = ax.reshape(6)
+for i in range(6):
+    ax[i].plot(t, q[:, i], label='q')
+    ax[i].plot(t, q_des[:, i], label='q_des', c='r', ls='--')
+    ax[i].set_ylabel(f'q_{i} (rad)')
+    ax[i].set_xlabel('Time (s)')
+    ax[i].legend()
+
+fig, ax = plot_utils.create_empty_figure(3, 2)
+ax = ax.reshape(6)
+for i in range(6):
+    ax[i].plot(t, qd[:, i], label='qd')
+    ax[i].plot(t, qd_des[:, i], label='qd_des', c='r', ls='--')
+    ax[i].set_ylabel(f'qd_{i} (rad/s)')
+    ax[i].set_xlabel('Time (s)')
+    ax[i].legend()
+
+fig, ax = plot_utils.create_empty_figure(3, 2)
+ax = ax.reshape(6)
+for i in range(6):
+    ax[i].plot(t, qdd[:, i], label='qdd')
+    ax[i].plot(t, qdd_des[:, i], label='qdd_des', c='r', ls='--')
+    ax[i].set_ylabel(f'qdd_{i} (rad/s^2)')
+    ax[i].set_xlabel('Time (s)')
+    ax[i].legend()
+
+fig, ax = plot_utils.create_empty_figure(3, 2)
+ax = ax.reshape(6)
+for i in range(6):
+    ax[i].plot(t, tau[:, i], label='tau')
+    ax[i].plot(t, tau_des[:, i], label='tau_des', c='r', ls='--')
+    ax[i].set_ylabel(f'tau_{i} (Nm)')
+    ax[i].set_xlabel('Time (s)')
+    ax[i].legend()
+
+plt.show()
