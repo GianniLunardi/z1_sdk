7aad6b399fe07d6b6796c3b79bcef735f740e5dc
GianniLunardi <gianni.lunardi96@gmail.com>
2024-11-13 14:06:57 +0100

    analysis of the difference btw measured torque and computed by dynamics (model)

--- scripts/regression.py
+++ scripts/regression.py
@@ -20,11 +20,6 @@ def bias(q_m, qdd_m, tau_m):
     M = kin_dyn.mass_matrix(H_b, q_m)[6:, 6:]
     g = kin_dyn.gravity_term(H_b, q_m)[6:]
     return tau_m[j] - M[j, j] * qdd_m[j] - g[j]
-    
-
-def regressor(qd_m, qdd_m):
-    # Compute the regressor vector
-    return np.array([qdd_m[j], qd_m[j], np.tanh(1e3 * qd_m[j])])
 
 
 j = 0 if len(sys.argv) < 2 else int(sys.argv[1])
@@ -32,9 +27,12 @@ if j < 0 or j > 5:
     print('Invalid joint index. Please provide a joint index between 0 and 5.')
     sys.exit(1)
 
+WITH_ROTOR = 1 if j in [4, 5] else 0
+
 dt = 2e-3
-state_log = pd.read_csv(f'../data/state_log_{j}.csv')
-filt_log = np.load(f'../data/filt_log_{j}.npz')
+folder = '../data_bang/'
+state_log = pd.read_csv(f'{folder}state_log_{j}.csv')
+filt_log = np.load(f'{folder}filt_log_{j}.npz')
 state_array = state_log.to_numpy()
 q = state_array[:, 0:6]
 n = len(q)
@@ -45,7 +43,7 @@ qd[:, j], qdd[:, j], tau[:, j] = filt_log['qd'], filt_log['qdd'], filt_log['tau'
 urdf_name = '../z1.urdf'
 robot = URDF.from_xml_file(urdf_name)
 
-robot_joints = robot.joints[1:]         # skip the root joint
+robot_joints = robot.joints[1:7]         # skip the root joint
 joint_names = [joint.name for joint in robot_joints]
 kin_dyn = KinDynComputations(urdf_name, joint_names, robot.get_root())
 kin_dyn.set_frame_velocity_representation(adam.Representations.BODY_FIXED_REPRESENTATION)
@@ -57,25 +55,28 @@ for i in range(n):
     tau_comp[i] = computed_torque(q[i], qd[i], qdd[i])[j]
 
 # Compute the regressor matrix and the bias vector
+p = 3 if WITH_ROTOR else 2
 bias_vector = np.zeros((n, 1))
-regressor_matrix = np.zeros((n, 3))
+regressor_matrix = np.zeros((n, p))
+mjj = np.zeros(n)
 for i in range(n):
-    temp =bias(q[i], qdd[i], tau[i])
     bias_vector[i] = bias(q[i], qdd[i], tau[i])
-    regressor_matrix[i] = regressor(qd[i], q[i])
+    reg = np.array([qd[i, j], np.tanh(1e2 * qd[i, j]), qdd[i, j]]) 
+    regressor_matrix[i] = reg if WITH_ROTOR else reg[:-1]
 # Solve the LS problem
 params = np.linalg.pinv(regressor_matrix) @ bias_vector
-# Order --> [k^2 * I_r, F_v, F_s]
-print('Regressor parameters: ', params)
+if not WITH_ROTOR:
+    params = np.vstack((params, 0.))
+print(f'Regressor parameters: \nF_v = {params[0]}\nF_s = {params[1]}\nk^2 * I_r = {params[2]}')
 
-tau_w_fric = np.copy(tau_comp)
-tau_w_fric += params[1] * qd[:, j] + params[2] * np.sign(qd[:, j])
+tau_w_param = np.copy(tau_comp)
+tau_w_param += params[0] * qd[:, j] + params[1] * np.sign(qd[:, j]) + params[2] * qdd[:, j]
 
 t = np.linspace(0, n * dt, n)
 plt.figure()
 plt.plot(t, tau[:, j], label='Measured torque')
 plt.plot(t, tau_comp, label='Computed torque', c='r', ls='--')
-plt.plot(t, tau_w_fric, label='With friction', c='g', ls='--')
+plt.plot(t, tau_w_param, label='With param', c='g', ls='--')
 plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Torque (Nm)')
@@ -83,3 +84,9 @@ plt.title(f'Joint {j} Torque comparison')
 
 
 plt.show()
+
+
+### RESULTS
+# rotor_inertia = np.array([0., 0., 0., 0., 0.03290668, 0.00473329])[:nq]
+# f_viscous = np.array([3.11502837, 7.51791126, 2.63321954, 2.73281554, 2.62967973, 2.64751243])[:nq]
+# f_static = np.array([1.01857202, 2.19954325, 1.69762618, 1.22486538, 0.9563537, 1.03245635])[:nq]
--- scripts/signal_from_exp.py
+++ scripts/signal_from_exp.py
@@ -1,11 +1,15 @@
 import sys
+import adam.numpy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from orc.utils import plot_utils
-import example_robot_data
 import scipy.signal as signal
 from scipy.fftpack import fft
+from urdf_parser_py.urdf import URDF
+import adam
+import unitree_arm_interface
+import pinocchio as pin 
 
 
 def filtering_signal(ts, order, cut_off):
@@ -17,8 +21,21 @@ j = 0 if len(sys.argv) < 2 else int(sys.argv[1])
 if j < 0 or j > 5:
     print('Invalid joint index. Please provide a joint index between 0 and 5.')
     sys.exit(1)
+
+urdf_name = '../z1.urdf'
+robot = URDF.from_xml_file(urdf_name)
+
+arm =  unitree_arm_interface.ArmInterface(hasGripper = True)
+armModel = arm._ctrlComp.armModel
+
+robot_joints = robot.joints[1:7]         # skip the root joint
+joint_names = [joint.name for joint in robot_joints]
+kin_dyn = adam.numpy.KinDynComputations(urdf_name, joint_names, robot.get_root())
+kin_dyn.set_frame_velocity_representation(adam.Representations.BODY_FIXED_REPRESENTATION)
+H_b = np.eye(4)                         # Roto-translation world --> base/root
     
-state_log = pd.read_csv(f'../data/state_log_{j}.csv')
+folder = '../data_bang/'
+state_log = pd.read_csv(f'{folder}state_log_{j}.csv')
 state_array = state_log.to_numpy()
 q = state_array[:, 0:6]
 qd = state_array[:, 6:12]
@@ -36,18 +53,36 @@ freq = np.fft.fftfreq(n, dt)
 qd_filt, qd_fft = filtering_signal(qd[:, j], order=2, cut_off=2)
 tau_filt, tau_fft = filtering_signal(tau[:, j], 2, 2)
 
-qdd = np.gradient(qd_filt, dt)
+qdd = np.zeros((n, 6))
+qdd[:, j] = np.gradient(qd_filt, dt)
+qd_raw = np.copy(qd[:, j])
+qd[:, j] = qd_filt
 
-np.savez(f'../data/filt_log_{j}.npz', qd=qd_filt, qdd=qdd, tau=tau_filt)
+np.savez(f'{folder}filt_log_{j}.npz', qd=qd_filt, qdd=qdd[:, j], tau=tau_filt)
 
 PLOT_POS = 0
 PLOT_VEL = 0
 PLOT_TORQUE = 0
 PLOT_FILT = 0
+PLOT_EE = 0
 
-robot = example_robot_data.load('z1')   
-rmodel = robot.model
+rmodel = pin.buildModelFromUrdf(urdf_name)
 rdata = rmodel.createData()
+
+frame_name = 'link06'
+ee_z1, ee_adam = np.zeros((n, 3)), np.zeros((n, 3))
+tau_z1, tau_adam = np.zeros((n, 6)), np.zeros((n, 6))
+for i in range(n):
+    # Forward kinematics
+    H_ee = kin_dyn.forward_kinematics(frame_name, H_b, q[i])
+    ee_adam[i] = H_ee[:3, 3]
+    H_ee = armModel.forwardKinematics(q[i], 5)
+    ee_z1[i] = H_ee[:3, 3]
+
+    # Inverse dynamics
+    tau_adam[i] = kin_dyn.mass_matrix(H_b, q[i])[6:, 6:] @ qdd[i] + \
+                  kin_dyn.bias_force(H_b, q[i], np.zeros(6), qd[i])[6:]
+    tau_z1[i] = armModel.inverseDynamics(q[i], qd[i], qdd[i], np.zeros(6))
 
 if PLOT_POS:
     fig, ax = plot_utils.create_empty_figure(3, 2)
@@ -75,21 +110,27 @@ if PLOT_TORQUE:
     for i in range(6):
         ax[i].axhline(rmodel.effortLimit[i], c='k', ls='--', lw=1)
         ax[i].axhline(-rmodel.effortLimit[i], c='k', ls='--', lw=1)
-        ax[i].plot(t, tau[:, i], label='tau' + str(i))
+        ax[i].plot(t, tau[:, i], label='tau_' + str(i))
+        ax[i].plot(t, tau_z1[:, i], label='z1_' + str(i), c='g', ls='--')
+        ax[i].plot(t, tau_adam[:, i], label='adam_' + str(i), c='r', ls='--')
         ax[i].set_ylabel(fr'$\tau_{i}$ (Nm)')
         ax[i].set_xlabel('Time (s)')
+        ax[i].legend()
 
 if PLOT_FILT:
     plt.figure()
-    plt.plot(t, qd[:, j], label='raw')
+    plt.plot(t, qd_raw, label='raw')
     plt.plot(t, qd_filt, label='filtered', c='r', ls='--')
-    plt.plot(t, qdd, label='qdd_fd', c='g')
+    plt.plot(t, qdd[:, j], label='qdd_fd', c='g')
     plt.xlabel('Time (s)')
     plt.ylabel('Velocity (rad/s)')
+    plt.legend()
+    plt.savefig(f'vel_acc_{j}.png')
 
     plt.figure()
     plt.plot(t, tau[:, j], label='raw')
-    plt.plot(t, tau_filt, label='filtered', c='r', ls='--')
+    plt.plot(t, tau_filt, label='filtered', c='g', ls='--')
+    plt.plot(t, tau_adam[:, j], label='adam_' + str(j), c='r', ls='--')
     plt.xlabel('Time (s)')
     plt.ylabel('Torque (Nm)')
 
@@ -97,6 +138,15 @@ if PLOT_FILT:
     plt.stem(freq, np.abs(tau_fft),'b', markerfmt=" ", basefmt="-b")
     plt.xlabel('Freq (Hz)')
     plt.ylabel('FFT Amplitude |X(freq)|')
+
+if PLOT_EE:
+    fig, ax = plot_utils.create_empty_figure(3, 1)
+    for i in range(3):
+        ax[i].plot(t, ee_z1[:, i], label='z1_' + str(i), c='g', ls='--')
+        ax[i].plot(t, ee_adam[:, i], label='adam_' + str(i), c='r', ls='--')
+        ax[i].set_ylabel(f'EE_{i} (m)')
+        ax[i].set_xlabel('Time (s)')
+        ax[i].legend()
 
 
 plt.show()
