060165153ae1340c5a41637fe0e1e7e73a2ea42a
GianniLunardi <gianni.lunardi96@gmail.com>
2025-02-17 14:49:03 +0100

    add 3d curve reference for ee, log data

--- mpc_experiments/ol_mpc_simu.py
+++ mpc_experiments/ol_mpc_simu.py
@@ -7,6 +7,25 @@ from safe_mpc.utils import get_ocp, get_controller
 from safe_mpc.controller import SafeBackupController
 
 
+def lemniscate(t):
+    x = a * np.sin(omega * t) 
+    y = a * np.sin(omega * t) * np.cos(omega * t)
+    z = b * np.cos(omega * t) - b
+    return np.array([x, y, z])
+
+def twisted_lemniscate(t):
+    x = a * np.cos(omega * t) 
+    y = a * np.sin(omega * 2 * t)
+    z = b * np.sin(omega * t)
+    return np.array([x, y, z])
+
+def mobius_inspired(t):
+    x = a * np.cos(omega * t)
+    y = b * np.sin(omega * t) * np.cos(omega * t / 2)
+    z = b * np.sin(omega * t) * np.sin(omega * t / 2)
+    return np.array([x, y, z])
+
+
 args = parse_args()
 model_name = args['system']
 params = Parameters(model_name, rti=True, filename='config.yaml')
@@ -14,6 +33,22 @@ params.build = args['build']
 params.act = args['activation']
 model = AdamModel(params, n_dofs=6)
 nq = model.nq
+
+q0 = np.array([0., 0.26178, -0.26178, 0., 0., 0.])
+q0 = q0[:nq]
+x0 = np.zeros(model.nx)
+x0[:nq] = q0
+
+use_3d_curve = 1
+a, b, omega = 0.1, 0.1, 2 * np.pi * 0.1
+if use_3d_curve:
+    ee_ref = np.array([0.42, 0., 0.378])
+    if use_3d_curve == 1:
+        ee_ref += lemniscate(0.)
+    elif use_3d_curve == 2:
+        ee_ref += twisted_lemniscate(0.)
+    else:
+        ee_ref += mobius_inspired(0.)
 model.ee_ref = ee_ref
 
 cont_name = args['controller']
@@ -26,7 +61,6 @@ opts = {
         'ipopt.tol': 1e-6,
         'ipopt.constr_viol_tol': 1e-6,
         'ipopt.compl_inf_tol': 1e-6,
-        'ipopt.linear_solver': 'ma57',
         'ipopt.hessian_approximation': 'limited-memory',
         'ipopt.max_iter': params.nlp_max_iter
         }
@@ -37,11 +71,6 @@ safe_ocp = SafeBackupController(model, obstacles)
 if args['build']:
     print('*** Ready for running the MPC at the next launch ***')
     exit()
-
-q0 = np.array([0., 0.26178, -0.26178, 0., 0., 0.])
-q0 = q0[:nq]
-x0 = np.zeros(model.nx)
-x0[:nq] = q0
 
 # Initial guess
 print('\n', '*'*5, 'WARM START', '*'*5, '\n')
@@ -81,15 +110,28 @@ for i in range(params.n_steps):
 
     start_time = time.time()
 
-    if i == 500:
-        ee_ref = np.array([0.3, 0., 0.478])
-        controller.setReference(ee_ref)
-        rviz.setTarget(ee_ref)
+    if use_3d_curve:
+        if use_3d_curve == 1:
+            curve_ref = lemniscate(i * params.dt)
+        elif use_3d_curve == 2:
+            curve_ref = twisted_lemniscate(i * params.dt)
+        else:
+            curve_ref = mobius_inspired(i * params.dt)
+        curve_ref += ee_ref
+        controller.setReference(curve_ref)
+        if i % 5 == 0:
+            rviz.setTarget(curve_ref)
+    else:
 
-    if i == 1000:
-        ee_ref = np.array([0.2, -0.28, 0.378])
-        controller.setReference(ee_ref)
-        rviz.setTarget(ee_ref)
+        if i == 500:
+            ee_ref = np.array([0.3, 0., 0.478])
+            controller.setReference(ee_ref)
+            rviz.setTarget(ee_ref)
+
+        if i == 1000:
+            ee_ref = np.array([0.2, -0.28, 0.378])
+            controller.setReference(ee_ref)
+            rviz.setTarget(ee_ref)
 
     if sa_flag and ia < safe_ocp.N:
         u[i] = u_abort[ia]
@@ -126,7 +168,7 @@ for i in range(params.n_steps):
     delta = params.dt - (end_time - start_time)
     solver_time[i] = controller.ocp_solver.get_stats("time_tot")
     tot_time[i] = end_time - start_time
-    if(i%3==0):
+    if i % 3 == 0:
         print(f'Iteration {i+1}/{params.n_steps} - '
               f'Time: {tot_time[i]:.3f}s - '
                 f'Total solver time {solver_time[i]:.3f}s \n')
@@ -142,3 +184,6 @@ print(f'{int(perc * 100)} percentile, tot = {np.quantile(tot_time, perc):.3f}s, 
       f'solver = {np.quantile(solver_time, perc)}')
 print(f'Max time, tot = {max(tot_time):.3f}s, '
       f'solver = {max(solver_time)}')
+
+# Save data
+np.savez_compressed('data/mpc_simu.npz', x=x, u=u)
